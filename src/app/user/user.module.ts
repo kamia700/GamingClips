8217e1b9264f965839861fc36ded31edc48da69c
Anastasia Kaminetc <a.kaminetc@innopolis.ru>
2023-02-25 11:30:23 +0300

    added reactive form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthMadalComponent } from './auth-madal/auth-madal.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AuthMadalComponent
+    AuthMadalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     AuthMadalComponent
